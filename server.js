58815c978c3b5afbc6e29bf9a87b2946864a1cb1
Emil Pirfält <emil.pirfalt@jayway.com>
2019-12-13 13:48:13 +0100

    Added dockerfile

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const http = require('http');
+const http = require("http");
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/plain");
   const data = {
-    "value":"hello world",
-    "author":"Johanna"
-  }
-  const response = JSON.stringify(data)
+    value: "hello world",
+    author: "Johanna"
+  };
+  const response = JSON.stringify(data);
   res.end(response);
 });
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
+});
+
+process.on("SIGINT", () => {
+  console.log(`process received a SIGINT signal`);
+  server.close();
 });
