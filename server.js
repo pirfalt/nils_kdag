88eedfbf085cf5e6e04a7bf93d8e02a044da5275
Michelle Khan <michelle.khan1@outlook.com>
2019-12-13 15:52:56 +0100

    Installed express

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
-const http = require("http");
+const express = require('express')
+const app = express()
 const port = 3000;
 
 let data = {
-  
-  "/nils" : {
-    mail : "Nils epostadress",
-    firstname : "Nils",
-    lastname : "Persson"
+
+  "nils": {
+    mail: "Nils epostadress",
+    firstname: "Nils",
+    lastname: "Persson"
   },
-  "/emil" : {
-    mail : "Emils epostadress",
-    firstname : "Emil",
-    lastname : "Pirfält"
+  "emil": {
+    mail: "Emils epostadress",
+    firstname: "Emil",
+    lastname: "Pirfält"
+  },
+  "michelle": {
+    mail: "Michelles mail",
+    firstname: "Michelle",
+    lastname: "Khan"
   }
 };
 
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-  //console.log(req.headers);
-  let response = data[req.url]
-  if(response==null) {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(JSON.stringify("Sorry kunde inte hitta: "+req.url));
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/:username/firstname', function (req, res) {
+  const user = data[req.params.username];
+  if (user == null) {
     return;
   }
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(JSON.stringify(response));
-});
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  const response = user.firstname;
+  if (response == null) {
+    return;
+  }
+  res.json(response);
+})
+
+app.get('/:username', function (req, res) {
+  const user = data[req.params.username];
+  if (user == null) {
+    return;
+  }
+  res.json(user);
+})
+
+app.listen(port)
 
 process.on("SIGINT", () => {
   console.log(`process received a SIGINT signal`);
