c16fc10ceaca65eada3f6014b1681d892a8754d8
Johanna Levall <johanna.levall@jayway.com>
2019-12-13 12:35:21 +0000

    Uppdatering

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const data = {"value":"hello world, this is Johanna"}
+  const data = {
+    "value":"hello world",
+    "author":"Johanna"
+  }
   const response = JSON.stringify(data)
   res.end(response);
 });
