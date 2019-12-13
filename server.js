fd8b8f9d8e0d34493e83d036c6603ea27d383998
Karolina Rantfors <karolina.rantfors@jayway.com>
2019-12-13 10:10:31 +0000

    bytt fran HTTP till JSON format

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end({"value":"hello world"});
 });
 server.listen(port, () => {
   console.log(`Server running at http://${port}/`);
