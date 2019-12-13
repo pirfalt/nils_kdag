3f03f3e308859efaa216eeda7667e1ab8af9810a
Nils <nils.3.persson@gmail.com>
2019-12-13 14:30:31 +0000

    Med battre svar

--- server.js
+++ server.js
@@ -2,20 +2,33 @@ const http = require("http");
 const port = 3000;
 
 let data = {
-  value: "hello world",
-  author: "Johanna"
+  
+  "/nils" : {
+    mail : "Nils epostadress",
+    firstname : "Nils",
+    lastname : "Persson"
+  },
+  "/emil" : {
+    mail : "Emils epostadress",
+    firstname : "Emil",
+    lastname : "Pirfält"
+  }
 };
 
 const server = http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.url);
-  console.log(req.headers);
-
+  //console.log(req.headers);
+  let response = data[req.url]
+  if(response==null) {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(JSON.stringify("Sorry kunde inte hitta: "+req.url));
+    return;
+  }
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-
-  const response = JSON.stringify(data);
-  res.end(response);
+  res.end(JSON.stringify(response));
 });
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
