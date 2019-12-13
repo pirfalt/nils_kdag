b4b7bdf211f5d501a4bfb0b97a791a92020e85c2
nicklassundvisson <nicklas.sundvisson@jayway.com>
2019-12-13 10:18:24 +0000

    Nicklas testar json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end({"value":"hello world"});
+  res.end({"value":"hello world, this is nicklas"});
 });
 server.listen(port, () => {
   console.log(`Server running at http://${port}/`);
