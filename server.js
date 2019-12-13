82c9db1534928b6dc27851c4a5d64ac530ff1fb6
Johanna Levall <johanna.levall@jayway.com>
2019-12-13 12:14:39 +0000

    Ny

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  //res.end('Hello World\n');
-  res.end('{"value": "Hello World!"}');
+  const data = {"value":"hello world, this is Johanna"}
+  const response = JSON.stringify(data)
+  res.end(response);
 });
 server.listen(port, () => {
-  console.log(`Server running at http://:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-//test push!
