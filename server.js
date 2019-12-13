67ec2cdb793ec4e79faa173cf68553e3b1e24b4a
Emil Pirfält <emil.pirfalt@jayway.com>
2019-12-13 14:43:15 +0100

    log requests

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const http = require("http");
 const port = 3000;
+
+let data = {
+  value: "hello world",
+  author: "Johanna"
+};
+
 const server = http.createServer((req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.headers);
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  const data = {
-    value: "hello world",
-    author: "Johanna"
-  };
+
   const response = JSON.stringify(data);
   res.end(response);
 });
@@ -16,5 +23,8 @@ server.listen(port, () => {
 
 process.on("SIGINT", () => {
   console.log(`process received a SIGINT signal`);
-  server.close();
+  server.close(err => {
+    console.log(err);
+    process.exit();
+  });
 });
